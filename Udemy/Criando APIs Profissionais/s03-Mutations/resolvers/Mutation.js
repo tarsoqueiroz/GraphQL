7c80a113d3f8158ab18aff550479dc0d7621b322
Tarso Queiroz <tarsoqueiroz@gmail.com>
2020-01-28 01:27:28 -0300

    Changing location and goto lesson about Mutation

---
+++ Udemy/Criando APIs Profissionais/s03-Mutations/resolvers/Mutation.js
@@ -0,0 +1,30 @@
+const { usuarios, proximoId } = require('../data/db')
+
+module.exports = {
+    novoUsuario(_, args) { //  args = { nome, email, idade }
+      const emailExistente = usuarios
+        .some(u => u.email === args.email)
+
+      if (emailExistente) {
+        throw new Error('E-mail cadastrado')
+      }
+
+      const novo = {
+      id: proximoId(),
+      ...args, //    nome, email, idade
+      perfil_id: 1,
+      status: 'ATIVO'
+    }
+
+    usuarios.push(novo)
+
+    return novo
+  },
+  excluirUsuario(_, { id }) {
+    const i = usuarios
+      .findIndex(u => u.id === id)
+    if (i < 0) return null
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
+  }
+}
